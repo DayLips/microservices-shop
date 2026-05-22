9f0e09411bcecf03f0d97ecea7bb194b9f26e992
DayLips <daylips312@gmail.com>
2026-05-22 18:45:47 +0300

    Finnaly api-gateway

--- api-gateway/config/urls.py
+++ api-gateway/config/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(requset):
+    return JsonResponse({
+        'status': 'healthy',
+        'service': 'api-gateway',
+        'services': {
+            'user-service': 'http://localhost:8004',
+            'product-service': 'http://localhost:8001',
+            'cart-service': 'http://localhost:8002',
+            'order-service': 'http://localhost:8003',
+        }
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check),
+    path('api/', include('apps.gateway.urls'))
 ]
